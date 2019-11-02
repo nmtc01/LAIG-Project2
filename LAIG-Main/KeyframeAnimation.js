f3c2357fceb6d18cc030f4c0cdd3e611d5709406
Gustavo nRm <gustavonrm@gmail.com>
2019-11-02 19:21:40 +0000

    translate 💯

--- LAIG-Main/KeyframeAnimation.js
+++ LAIG-Main/KeyframeAnimation.js
@@ -23,7 +23,22 @@ class KeyFrameAnimaton extends Animation {
         this.m = mat4.create();
 
         //translate -> rotate -> scale 
-        this.keyframes = keyframes;
+        this.keyframes = [
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [1, 1, 1],
+                0
+            ]
+        ]; //keyframe 0 instant 
+        
+        for(let i =0;i < keyframes.length; i++){
+            this.keyframes.push(keyframes[i]);
+            console.log(i);
+        }
+       
+        console.log(this.keyframes);
+        //this.keyframes= keyframes;
 
         this.instant = this.keyframes[0][3]; //first keyframe passed with instant stored in 3rd index
 
@@ -32,13 +47,13 @@ class KeyFrameAnimaton extends Animation {
         //make segment time array 
         this.t = [this.instant];
         for (let i = 1; i < this.keyframes.length; i++) {
-            this.t.push(this.keyframes[i][3] - this.keyframes[i-1][3]);
+            this.t.push(this.keyframes[i][3] - this.keyframes[i - 1][3]);
         }
         //NOTE first keyfram is set with sample values 
     }
 
-    set_mn(mn){
-       this.mn = mn;
+    set_mn(mn) {
+        this.mn = mn;
     }
 
     /**
@@ -47,7 +62,7 @@ class KeyFrameAnimaton extends Animation {
     * @param {int} keyframe_num keyframe number operating
     */
     process_animation() {
-       
+
         //stop excecution 
         if (this.segment > this.keyframes.length - 1) {
             console.log('nao da mais');
@@ -60,57 +75,64 @@ class KeyFrameAnimaton extends Animation {
         if (this.sent > this.t[this.segment]) { //this.keyframes[this.segment][1] == t[segment]  
             this.sent -= this.t[this.segment]; // reset sent 
             this.segment++;
-            if (this.segment  > this.keyframes.length - 1) {
+            if (this.segment > this.keyframes.length - 1) {
                 console.log('nao da mais');
                 return this.m;
             }
         }
-        
+
         this.progress_percentage = this.sent / this.t[this.segment]; //percentage
-    
+
         //TRANSLATE
+        //calculate translation vector
         let T = [
-            this.keyframes[this.segment][0][0]*this.progress_percentage,
-            this.keyframes[this.segment][0][1]*this.progress_percentage,
-            this.keyframes[this.segment][0][2]*this.progress_percentage
+            (this.keyframes[this.segment][0][0] - this.keyframes[this.segment-1][0][0]) * this.progress_percentage,
+            (this.keyframes[this.segment][0][1] - this.keyframes[this.segment-1][0][1]) * this.progress_percentage,
+            (this.keyframes[this.segment][0][2] - this.keyframes[this.segment-1][0][2]) * this.progress_percentage
         ];
+        //sum veector to intial pos 
+        T[0]+= this.keyframes[this.segment-1][0][0];
+        T[1]+= this.keyframes[this.segment-1][0][1];
+        T[2]+= this.keyframes[this.segment-1][0][2];
+
         let translate_matrix = mat4.create();
         translate_matrix = mat4.translate(translate_matrix, translate_matrix, T);
-       
-        //TODO ROTATE 
+
+        //ROTATE 
         let R = [
-            this.keyframes[this.segment][1][0]*this.progress_percentage,
-            this.keyframes[this.segment][1][1]*this.progress_percentage,
-            this.keyframes[this.segment][1][2]*this.progress_percentage
+            this.keyframes[this.segment][1][0] * this.progress_percentage,
+            this.keyframes[this.segment][1][1] * this.progress_percentage,
+            this.keyframes[this.segment][1][2] * this.progress_percentage
         ];
         let rotation_matrix = mat4.create();
-        rotation_matrix = mat4.rotate(rotation_matrix , rotation_matrix , R[0],  [1, 0, 0]);
-        rotation_matrix = mat4.rotate(rotation_matrix , rotation_matrix , R[1],  [0, 1, 0]);
-        rotation_matrix = mat4.rotate(rotation_matrix , rotation_matrix , R[2],  [0, 0, 1]);
+        rotation_matrix = mat4.rotate(rotation_matrix, rotation_matrix, R[0], [1, 0, 0]);
+        rotation_matrix = mat4.rotate(rotation_matrix, rotation_matrix, R[1], [0, 1, 0]);
+        rotation_matrix = mat4.rotate(rotation_matrix, rotation_matrix, R[2], [0, 0, 1]);
 
         //TODO SCALE 
         let S = [
-            this.keyframes[this.segment][2][0]*this.progress_percentage,
-            this.keyframes[this.segment][2][1]*this.progress_percentage,
-            this.keyframes[this.segment][2][2]*this.progress_percentage
+            this.keyframes[this.segment][2][0] * this.progress_percentage,
+            this.keyframes[this.segment][2][1] * this.progress_percentage,
+            this.keyframes[this.segment][2][2] * this.progress_percentage
         ];
         let scale_matrix = mat4.create();
         scale_matrix = mat4.scale(scale_matrix, scale_matrix, S);
-    
+
 
         //multiply all matrixes
-        let aux_mat = mat4.create(); 
-        aux_mat = mat4.multiply(aux_mat,aux_mat,scale_matrix);
-        aux_mat = mat4.multiply(aux_mat,aux_mat,rotation_matrix);
-        aux_mat = mat4.multiply(aux_mat,aux_mat,translate_matrix);
+        let aux_mat = mat4.create();
+        //aux_mat = mat4.multiply(aux_mat, aux_mat, scale_matrix);
+        aux_mat = mat4.multiply(aux_mat, aux_mat, rotation_matrix);
+        aux_mat = mat4.multiply(aux_mat, aux_mat, translate_matrix);
         //calculate matriz SRT 
         let ma = mat4.create();
-        ma = mat4.multiply(ma,ma,aux_mat);
+        ma = mat4.multiply(ma, ma, aux_mat);
 
-        this.m = mat4.multiply(this.m,ma,this.mn);
-      
-        return this.m;
+        this.m = mat4.multiply(this.m, ma, this.mn);
+
         
+        return this.m;
+
     }
 
 }
