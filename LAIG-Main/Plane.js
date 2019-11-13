0f8ca1a6da8845aec2022d6455cd86e3f8d591f0
Gustavo nRm <gustavonrm@gmail.com>
2019-11-13 18:14:33 +0000

    plane

--- LAIG-Main/Plane.js
+++ LAIG-Main/Plane.js
@@ -11,11 +11,26 @@ class Plane extends CGFobject{
         this.NPartsU= NPartsU; 
         this.NPartsV= NPartsV;
 
-        //this.initBuffers();
+        this.init();
     }
 
-    initBuffers(){
+    init(){
+        this.controlPoints = [
+                                [
+                                    [-1/2, 0, 1/2, 1],  
+                                    [-1/2, 0, -1/2, 1]
+                                ], 
+                                [
+                                    [1/2, 0, 1/2, 1],  
+                                    [1/2, 0, -1/2, 1] 
+                                ]
+                            ]; //Vector(2|3|4) [][]
+		this.surface = new CGFnurbsSurface(1, 1, this.controlPoints);
+		this.plane = new CGFnurbsObject(this.scene, this.NPartsU, this.NPartsV, this.surface);
+    }
 
+    display(){
+        this.plane.display();
     }
 
     updateTexCoords(lg_s, lg_t) {
