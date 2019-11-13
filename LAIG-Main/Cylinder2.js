d1059c01e6c402b64229be760ceb03630dbb8d0c
Nuno <ns.cardoso@hotmai.com>
2019-11-13 23:24:49 +0000

    Still not that good

--- LAIG-Main/Cylinder2.js
+++ LAIG-Main/Cylinder2.js
@@ -11,8 +11,8 @@ class Cylinder2 extends CGFobject{
         this.base= base;
         this.top= top; 
         this.height= height; 
-        this.NPartsU= slices/2; 
-        this.NPartsV= stacks;
+        this.NPartsU= stacks; 
+        this.NPartsV= slices/2;
         this.NPointsU= 2;
         this.NPointsV= 3;
 
@@ -20,60 +20,49 @@ class Cylinder2 extends CGFobject{
     }
 
     init(){
-        let d_theta = Math.PI/this.NPartsU;
-		let d_stack = this.height/this.NPartsV;
-		let d_radius = (this.base-this.top)/this.height;
-		
-		//angle starting values 
-        let theta1 = 0;	
-        let theta2 = Math.PI;
-		let radius = this.base;
-		
-        //arrays to store primitives data
-        this.controlPoints1 = [];
-        this.controlPoints2 = [];
-
-		for (let i = 0; i <= this.NPartsV; i++) {
-            let auxV1 = [];
-            let auxV2 = [];
-			for (let j = 0; j <= this.NPartsU; j++) {
-
-				//Normals
-				let nx1 = Math.cos(theta1); 
-                let ny1 = Math.sin(theta1);
-                let nx2 = Math.cos(theta2); 
-				let ny2 = Math.sin(theta2);
-
-				//Coordinates
-				let x1 = radius*nx1;
-                let y1 = -radius*ny1;
-                let x2 = radius*nx2;
-				let y2 = -radius*ny2;
-				let z = i*d_stack;
-
-				//Storing values
-                let auxU1 = [];
-                let auxU2 = [];
-                auxU1.push(x1, y1, z, 1);
-                auxU2.push(x2, y2, z, 1);
-                auxV1.push(auxU1);
-                auxV2.push(auxU2);
-				
-				//Preparing next iteration
-                theta1 += d_theta;
-                theta2 += d_theta;
-            }
-            this.controlPoints1.push(auxV1);
-            this.controlPoints2.push(auxV2);
-
-			//Preparing next iteration
-			radius -= d_radius*d_stack;
-            theta1 = 0;
-            theta2 = Math.PI;
-		}
-
         let degree1 = this.NPointsU - 1;
         let degree2 = this.NPointsV - 1;
+
+        this.controlPoints1 = [
+            //U0
+            [
+                //V0
+                [-this.base, 0, 0, 1],
+                //V1
+                [0, this.base, 0, 1],
+                //V2
+                [this.base, 0, 0, 1]
+            ],
+            //U1
+            [
+                //V0
+                [-this.base, 0, this.top, 1],
+                //V1
+                [0, this.base, this.top, 1],
+                //V2
+                [this.base, 0, this.top, 1]
+            ]
+        ];
+        this.controlPoints2 = [
+            //U0
+            [
+                //V0
+                [this.base, 0, 0, 1],
+                //V1
+                [0, -this.base, 0, 1],
+                //V2
+                [-this.base, 0, 0, 1]
+            ],
+            //U1
+            [
+                //V0
+                [this.base, 0, this.top, 1],
+                //V1
+                [0, -this.base, this.top, 1],
+                //V2
+                [-this.base, 0, this.top, 1]
+            ]
+        ];
 
         this.surface1 = new CGFnurbsSurface(degree1, degree2, this.controlPoints1);
         this.surface2 = new CGFnurbsSurface(degree1, degree2, this.controlPoints2);
